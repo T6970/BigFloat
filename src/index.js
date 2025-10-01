17ff2d1d929a629ec1008fa4dbe3afc50fb13f4c
T6970 <tenaldkyf315@gmail.com>
2025-10-01 19:11:44 +0800

    Fixed typos and bugs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 
 // Internal function
-// Highest common factor of two BigInts
+// Highest common factor (a.k.a Greatest Common Divisor) of two BigInts
 function hcf(a, b) {
   var result = a
   var d = b
@@ -12,23 +12,20 @@ function hcf(a, b) {
     result = temp;
   };
   
-  if (this.denominator < 0n) {
-    this.numerator   *= -1n;
-    this.denominator *= -1n;
-  }
-  
   return result;
 };
 
 
 export class bigSlash {
   constructor(value) {
+
+    if (["number","bigint"].includes(typeof value)) value = value.toString();
+
+    // Input validation
     if (typeof value === "string" && value.trim() === "")     throw new TypeError  ("Can't accept non-numeric values!"          );
     if (!["number","bigint","string"].includes(typeof value)) throw new TypeError  ("Can't accept non-numeric values!"          );
     if (value.split(".").length > 2)                          throw new SyntaxError("Numbers can't have multiple decimal point!");
     
-    if (["number","bigint"].includes(typeof value)) value = value.toString();
-
     this.denominator   = 1n;
     if (value.includes(".")) {
       this.denominator = 10n ** BigInt(value.split(".")[1].length);
@@ -36,15 +33,18 @@ export class bigSlash {
     this.numerator = BigInt(value.replace(/\./g, ""));
     
     let divide = hcf(this.numerator, this.denominator);
+    this.numerator   /= divide;
+    this.denominator /= divide;
     
-    this.numerator   = this.numerator   / divide
-    this.denominator = this.denominator / divide
+    if (this.denominator < 0n) {
+      this.numerator   *= -1n;
+      this.denominator *= -1n;
+    }
   },
 
   toString() {
-    if (this.denominator === 1) {eturn `${this.numerator};
+    if (this.denominator === 1n) return `${this.numerator}`;
     return `${this.numerator}/${this.denominator}`;
   },
 
 }
-
