48f1b7a18f44649d5bce927362bfd3c63b20207f
T6970 <tenaldkyf315@gmail.com>
2025-10-01 19:05:30 +0800

    Fix bugs and edge cases

--- src/index.js
+++ src/index.js
@@ -11,25 +11,40 @@ function hcf(a, b) {
     d = result % d;
     result = temp;
   };
+  
+  if (this.denominator < 0n) {
+    this.numerator   *= -1n;
+    this.denominator *= -1n;
+  }
+  
   return result;
 };
 
 
 export class bigSlash {
   constructor(value) {
-    if (value.split(".").length > 2) throw new SyntaxError("Numbers can't have multiple decimal point!");
+    if (typeof value === "string" && value.trim() === "")     throw new TypeError  ("Can't accept non-numeric values!"          );
+    if (!["number","bigint","string"].includes(typeof value)) throw new TypeError  ("Can't accept non-numeric values!"          );
+    if (value.split(".").length > 2)                          throw new SyntaxError("Numbers can't have multiple decimal point!");
     
-    if (value.contains(".")) {
-    const den = 10n ** BigInt(value.split(".")[1].length);
-    } else {
-    const den = 1n
+    if (["number","bigint"].includes(typeof value)) value = value.toString();
+
+    this.denominator   = 1n;
+    if (value.includes(".")) {
+      this.denominator = 10n ** BigInt(value.split(".")[1].length);
     };
-    const num = BigInt(value.replace(/\./g, ""));
+    this.numerator = BigInt(value.replace(/\./g, ""));
     
-    let divide = hcf(num, den);
+    let divide = hcf(this.numerator, this.denominator);
     
-    this.numerator   = num / divide
-    this.denominator = den / divide
-  };
+    this.numerator   = this.numerator   / divide
+    this.denominator = this.denominator / divide
+  },
+
+  toString() {
+    if (this.denominator === 1) {eturn `${this.numerator};
+    return `${this.numerator}/${this.denominator}`;
+  },
+
 }
 
