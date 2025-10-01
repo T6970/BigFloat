08b702a9623b290034dbe4615525d4eab52233e7
T6970 <tenaldkyf315@gmail.com>
2025-10-01 19:44:07 +0800

    Add fraction support

--- src/index.js
+++ src/index.js
@@ -19,13 +19,20 @@ function hcf(a, b) {
 export class bigSlash {
   constructor(value) {
 
-    if (["number","bigint"].includes(typeof value)) value = value.toString();
+    if (["number","bigint"].includes(typeof value)) value = value.toString().trim();
 
     // Input validation
     if (typeof value === "string" && value.trim() === "")     throw new TypeError  ("Can't accept non-numeric values!"          );
     if (!["number","bigint","string"].includes(typeof value)) throw new TypeError  ("Can't accept non-numeric values!"          );
     if (value.split(".").length > 2)                          throw new SyntaxError("Numbers can't have multiple decimal point!");
-    
+
+    if (value.includes("/")) {
+      if (value.includes("."))           throw new SyntaxError("Fractions cannot contain decimals!"               );
+      if (value.split("/").length !== 2) throw new SyntaxError("Fractions cannot contain multiple division signs!");
+      this.numerator   = value.split("/")[0];
+      this.denominator = value.split("/")[1];
+    }
+      
     this.denominator   = 1n;
     if (value.includes(".")) {
       this.denominator = 10n ** BigInt(value.split(".")[1].length);
